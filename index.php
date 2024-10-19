f74a2f6dc62def0c7f70de8f7db9df2820c72af3
Fabiola García Saravia <59809507+Mjscruse7@users.noreply.github.com>
2024-10-19 06:18:12 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Virtual Assistant BeIA</title>
+        <link rel="stylesheet" type="text/css" href="./css/style.css"/>
+        <script
+      src="https://static.soulmachines.com/widget-snippet-1.12.0.min.js"
+      data-sm-api-key="eyJzb3VsSWQiOiJkZG5hLW1mZ3MxYjlhLS1zb2x1dGlvbi1leHBlcnQiLCJhdXRoU2VydmVyIjoiaHR0cHM6Ly9kaC5zb3VsbWFjaGluZXMuY2xvdWQvYXBpL2p3dCIsImF1dGhUb2tlbiI6ImFwaWtleV92MV8wY2Y0MmUxNi02OTZhLTRlNDQtYmViOC0wMWE5ODc1ZDRmNjgifQ=="
+      data-sm-position="bottomRight"
+      data-sm-greeting="Welcome"
+      data-sm-layout="fullFrame"
+      data-sm-profile-picture="https://www.schonerfantasy.mjscruse7.com/BeIA.png"
+    ></script>
+    </head>
+    <body>
+        <div id="header">Virtual Assistant BeIA</div>
+        <div id="contenido">
+        Welcome!</br><br/>I am your virtual assistant, I am here to help you</div>
+    <footer>Belcorp Hackaton 5.0&#42; &#45; Pythonisas</footer>
+    </body>
+</html>
